3126c6779126fbe55b5657180fbf548fa3138c3f
jeewats <59193702+jeewats@users.noreply.github.com>
2020-08-27 16:22:06 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php 
-  $access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+  $access_token = 'nvKxWFmgwTVFM2Xs2a6dNfO2myEOUZe8hieYkUgAF2MSlSZ+ZMPT/HQMIaUwoXBF86przIhx1a5BlEmgglpIIihe/bxvsJ0FPuSn0gR0QpFQWeVR6Rt0IVpbLLpGGK6fXe63uX0pPd0sJUcLS5VchAdB04t89/1O/w1cDnyilFU=';
   $url = 'https://api.line.me/v1/oauth/verify';
   $headers = array('Authorization: Bearer ' . $access_token);
   $ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
